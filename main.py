513e4d4c81ef0c8ff454d5e0964a670a633f8669
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-21 01:39:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from PIL import Image
 import numpy as np
 
 class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
-with open("styles.css") as f:
-    custom_css = f.read()
 st.title('Iris classification based on sepal and petal size')
-st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
 
 st.image("image.jpg", use_column_width=True)
 input = open('lrc_iris.pkl', 'rb')
